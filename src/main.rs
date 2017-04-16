20ed06962c980637660940177b9c6bca2e50dcac
dreamersdw <dreamersdw@gmail.com>
2017-04-16 18:06:38 +0800

    Impl adding datapoints to the first archive

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::borrow::Cow;
 use std::clone::Clone;
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{Write, Error, Seek, SeekFrom};
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write, Error, Seek, SeekFrom};
 use std::iter::Iterator;
 use std::mem;
 use std::slice;
@@ -89,6 +89,7 @@ impl<'a> Iterator for TsIntoIterator<'a> {
     }
 }
 
+#[derive(Debug)]
 enum Aggregation {
     Avg,
     Max,
@@ -97,10 +98,12 @@ enum Aggregation {
     Last,
 }
 
+#[derive(Debug)]
 struct Meta {
     magic_number: [u8; 5],
     aggregation_method: Aggregation,
     max_rentention: u32,
+    num_of_archives: u32,
 }
 
 fn unix_time() -> u64 {
@@ -110,23 +113,26 @@ fn unix_time() -> u64 {
     }
 }
 
+#[derive(Debug)]
 struct ArchiveSpec {
     secs_per_point: u32,
     num_of_points: u32,
 }
 
+#[derive(Debug)]
 struct ArchiveInfo {
     secs_per_point: u32,
     num_of_points: u32,
     offset: usize,
 }
 
-struct RRD {
-    path: &'static str,
+struct Header {
+    meta: Meta,
+    archives: Vec<ArchiveInfo>,
 }
 
-unsafe fn as_bytes<'a, T>(val: T) -> &'a [u8] {
-    slice::from_raw_parts(&val as *const T as *const u8, mem::size_of::<T>())
+struct RRD {
+    path: &'static str,
 }
 
 impl RRD {
@@ -137,7 +143,7 @@ impl RRD {
     fn init_file(self, archives: &[ArchiveSpec], method: Aggregation) -> Result<(), Error> {
         let mut fd = File::create(self.path)?;
 
-        let mut total_size = 0;
+        let mut total_size: u64 = 0;
         let meta_size: usize;
 
         // write header meta
@@ -146,8 +152,10 @@ impl RRD {
             magic_number: *b"cello",
             aggregation_method: method,
             max_rentention: oldest,
+            num_of_archives: archives.len() as u32,
         };
-        let meta_bytes = unsafe { as_bytes(meta) };
+
+        let meta_bytes = unsafe { as_bytes(&meta) };
         fd.write(meta_bytes)?;
         meta_size = meta_bytes.len();
 
@@ -159,9 +167,9 @@ impl RRD {
                 secs_per_point: spec.secs_per_point,
                 offset: offset,
             };
-            let archive_bytes = unsafe { as_bytes(archive_info) };
+            let archive_bytes = unsafe { as_bytes(&archive_info) };
             fd.write(archive_bytes)?;
-            total_size += archive_bytes.len();
+            total_size += archive_bytes.len() as u64;
 
             offset += spec.num_of_points as usize * mem::size_of::<DataPoint>();
         }
@@ -171,37 +179,119 @@ impl RRD {
             .map(|x| x.num_of_points * mem::size_of::<DataPoint>() as u32)
             .fold(0, |sum, i| sum + i);
 
-        total_size += point_size as usize;
-        fd.seek(SeekFrom::Start(total_size as u64 - 1))?;
-        fd.write(&[0x0])?;
+        total_size += point_size as u64;
+        fd.set_len(total_size).unwrap();
         fd.flush()
     }
 
-    #[allow(dead_code)]
-    fn add_point(_: DataPoint) -> Result<(), Error> {
-        unimplemented!()
+    fn read_header(&self) -> Result<Header, Error> {
+        let mut fd = File::open(self.path)?;
+        let meta: Meta = unsafe { from_bytes(&mut fd) };
+        let mut archives: Vec<ArchiveInfo> = Vec::with_capacity(meta.num_of_archives as usize);
+
+        for _ in 0..meta.num_of_archives {
+            let archive: ArchiveInfo = unsafe { from_bytes(&mut fd) };
+            archives.push(archive);
+        }
+
+        Ok(Header {
+            meta: meta,
+            archives: archives,
+        })
+    }
+
+    fn add_point(&mut self, dp: DataPoint) -> Result<(), Error> {
+        let mut fd = OpenOptions::new().write(true)
+            .read(true)
+            .create(false)
+            .open(self.path)?;
+
+        let header = self.read_header()?;
+
+        let meta_size = mem::size_of::<Meta>();
+
+        for (i, archive) in header.archives.iter().enumerate() {
+            fd.seek(SeekFrom::Start(archive.offset as u64))?;
+            let last: DataPoint = unsafe { from_bytes(&mut fd) };
+            let current = DataPoint {
+                time: dp.time - (dp.time % archive.secs_per_point as u64),
+                value: dp.value,
+            };
+
+            // this is the fist datapoint
+            if last.time == 0 {
+                fd.seek(SeekFrom::Start(archive.offset as u64))?;
+                let bytes = unsafe { as_bytes(&current) };
+                fd.write(bytes)?;
+            } else {
+                let skiped_num = (current.time - last.time) / archive.secs_per_point as u64;
+                let skiped_offset = skiped_num * mem::size_of::<DataPoint>() as u64;
+                let new_offset = archive.offset as u64 +
+                                 skiped_offset as u64 %
+                                 (archive.num_of_points as u64 *
+                                  mem::size_of::<DataPoint>() as u64);
+                fd.seek(SeekFrom::Start(new_offset as u64))?;
+                let bytes = unsafe { as_bytes(&current) };
+                fd.write(bytes)?;
+
+                let archive_offset = meta_size as u64 +
+                                     i as u64 * mem::size_of::<ArchiveInfo>() as u64;
+                fd.seek(SeekFrom::Start(archive_offset))?;
+                let bytes = unsafe {
+                    as_bytes(&ArchiveInfo {
+                        num_of_points: archive.num_of_points,
+                        secs_per_point: archive.secs_per_point,
+                        offset: new_offset as usize,
+                    })
+                };
+                fd.write(bytes)?;
+            }
+        }
+
+        fd.flush()?;
+        Ok(())
     }
 }
 
-fn main() {
-    let f = RRD::new("example.rrd");
+unsafe fn as_bytes<'a, T>(val: &T) -> &'a [u8] {
+    slice::from_raw_parts(val as *const T as *const u8, mem::size_of::<T>())
+}
 
-    f.init_file(&[ArchiveSpec {
-                         secs_per_point: 60,
-                         num_of_points: 86400 * 1 / 60,
-                     },
-                     ArchiveSpec {
-                         secs_per_point: 60 * 5,
-                         num_of_points: 86400 * 7 / 300,
-                     },
-                     ArchiveSpec {
-                         secs_per_point: 60 * 30,
-                         num_of_points: 86400 * 30 / 1800,
-                     },
-                     ArchiveSpec {
-                         secs_per_point: 60 * 60 * 12,
-                         num_of_points: 86400 * 365 / 43200,
-                     }],
-                   Aggregation::Avg)
-        .expect("unable to create file");
+unsafe fn from_bytes<T>(reader: &mut File) -> T {
+    let mut val: T = mem::uninitialized();
+    let t_size = mem::size_of::<T>();
+    let t_slice = slice::from_raw_parts_mut(&mut val as *mut T as *mut u8, t_size);
+
+    reader.read_exact(t_slice).unwrap();
+    val
+}
+
+fn main() {}
+
+#[test]
+fn test_read_write_file() {
+    let rrd = RRD::new("/tmp/example.rrd");
+    let result = rrd.init_file(&[ArchiveSpec {
+                                     secs_per_point: 60,
+                                     num_of_points: 86400 * 1 / 60,
+                                 }],
+                               Aggregation::Avg);
+    assert!(result.is_ok());
+
+    let rrd = RRD::new("/tmp/example.rrd");
+    let result = rrd.read_header();
+    assert!(result.is_ok());
+    let header = result.unwrap();
+    assert_eq!(header.meta.num_of_archives, 1);
+    assert_eq!(header.archives.len(), 1);
+
+
+    let mut rrd = RRD::new("/tmp/example.rrd");
+    for i in 0..60 * 48 {
+        let result = rrd.add_point(DataPoint {
+            time: unix_time() + i * 60,
+            value: 12.3,
+        });
+        assert!(result.is_ok());
+    }
 }
